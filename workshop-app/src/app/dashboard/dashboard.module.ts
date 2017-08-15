584873a640d82d3acc34516c2bef3ed858cb1e66
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2017-08-15 13:31:37 -0400

    Load routes from the video module
    Signed-off-by: Kartik Null Cating-Subramanian <ksubramanian@chef.io>

--- workshop-app/src/app/dashboard/dashboard.module.ts
+++ workshop-app/src/app/dashboard/dashboard.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
 import { VideoDashboardComponent } from './video-dashboard/video-dashboard.component';
 import { VideoListComponent } from './video-list/video-list.component';
 import { VideoPlayerComponent } from './video-player/video-player.component';
 import { StatFiltersComponent } from './stat-filters/stat-filters.component';
 
+const routes: Routes = [
+  { path: '', component: VideoDashboardComponent, }
+];
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
   declarations: [
     VideoDashboardComponent,
